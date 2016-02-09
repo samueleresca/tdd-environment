905451bddae87c66e6eed1b5993a75623532e513
Samuele Resca <samuele.resca@gmail.com>
2016-02-09 22:34:55 +0100

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+//Grunt configuration
+module.exports = function(grunt) {
+  'use strict';
+  require('load-grunt-tasks')(grunt);
+  grunt.initConfig({
+    //blanket_mocha CONFIGURATION
+    blanket_mocha: {
+      options: {
+        run: true,
+        reporter: 'Min',
+        // We want a minimum of 70% coverage
+        threshold: 70
+      },
+      files: {
+        src: '*.html'
+      }
+    },
+
+
+    //KARMA CONFIGURATION
+    karma : {
+      options: {
+          //Karma config file
+          configFile: 'karma.conf.js',
+          files: [
+              'node_modules/chai/chai.js',
+              'node_modules/sinon-chai/lib/sinon-chai.js',
+              'node_modules/sinon/pkg/sinon.js',
+
+              // Our test files
+              'js/class-to-test.js',
+              'test/tests.js',
+              'test/index.html'
+          ]
+      },
+
+      dev: {
+          // All tested enviroments!!! (Browsers + phantomJS)
+          browsers: ['Chrome', 'Firefox', 'PhantomJS']
+      }
+  }
+  });
+  //Register the task as DEFUALT
+  grunt.registerTask('default', ['blanket_mocha']);
+};
